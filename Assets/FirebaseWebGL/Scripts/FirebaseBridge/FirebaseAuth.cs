91fe9dae039ae5f8d19952a872f5e2326ae9af23
Any3Develop <Any3Develop@Gmail.com>
2022-02-01 22:38:58 +0300

    Add auth anonymous

--- Assets/FirebaseWebGL/Scripts/FirebaseBridge/FirebaseAuth.cs
+++ Assets/FirebaseWebGL/Scripts/FirebaseBridge/FirebaseAuth.cs
@@ -5,6 +5,15 @@ namespace FirebaseWebGL.Scripts.FirebaseBridge
     public static class FirebaseAuth
     {
         /// <summary>
+        /// Creates and signs in a user anonymous
+        /// </summary>
+        /// <param name="objectName"> Name of the gameobject to call the callback/fallback of </param>
+        /// <param name="callback"> Name of the method to call when the operation was successful. Method must have signature: void Method(string output) </param>
+        /// <param name="fallback"> Name of the method to call when the operation was unsuccessful. Method must have signature: void Method(string output). Will return a serialized FirebaseError object </param>
+        [DllImport("__Internal")]
+        public static extern void SignInAnonymously(string objectName, string callback, string fallback);
+        
+        /// <summary>
         /// Creates a user with email and password
         /// </summary>
         /// <param name="email"> User email </param>
